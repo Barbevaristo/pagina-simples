0b998fd697a8740822d4e9725b70b1f7db93c120
Barbevaristo <barbevaristo@icloud.com>
2025-07-08 00:30:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("botaoMensagem").addEventListener("click", function() {
+    document.getElementById("mensagem").textContent = "Seja bem-vindo(a) à minha página! Nunca desista dos seus sonhos.";
+});
